bba705949cf92e5a3ba5a2286e9e2f5e8fcf980e
Joao Otavio Segantini <joaootaviosegantini@DESKTOP-LVUC041.localdomain>
2021-11-02 19:34:06 -0300

    Add HeroBlock

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,8 +1,13 @@
 import * as React from "react"
+import HeroBlock from "../components/home/heroBlock"
 //import { Link } from "gatsby"
 
 import Layout from "../components/ui/layout"
 
-const IndexPage = () => <Layout></Layout>
+const IndexPage = () => (
+  <Layout>
+    <HeroBlock />
+  </Layout>
+)
 
 export default IndexPage
